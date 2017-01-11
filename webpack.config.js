2491261d72a10198d35ca9387727e54b35181e98
fusheng <fusheng@netposa.com>
2017-01-11 16:22:09 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
       include: path.join(__dirname, 'src')
     },{
        test: /\.css$/,
-       loader:'style!css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+       loader:'style!css'
     }]
   }
 };
